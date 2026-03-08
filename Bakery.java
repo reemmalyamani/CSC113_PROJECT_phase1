ceeab4c3f717faf49309c20efc945ed3fd99aa7f
Reem Alyamani <alyamanireem@gmail.com>
2026-03-08 11:34:16 +0300

    Create Bakery class with properties and constructor

---
+++ Bakery.java
@@ -0,0 +1,13 @@
+public class Bakery {
+  private String name;
+  private String location;
+  private int nOfEmployee;
+  private int nOfCake;
+
+public Bakery (String name, String location, int nOfEmployee, int nOfCake){
+  this.name = name;
+  this.location = loacation;
+  this.nOfEmployee = nOfEmployee;
+  this.nOfCake = nOfCake;
+}
+}

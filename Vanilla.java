184b44d594bcd1fed997106711e921f855f1646b
Reem Alyamani <alyamanireem@gmail.com>
2026-03-08 12:38:22 +0300

    Implement Vanilla class with topping and methods
    Added Vanilla class extending Cake with properties and methods.

--- Vanilla.java
+++ Vanilla.java
@@ -1,1 +1,12 @@
-
+public class Vanilla extends Cake {
+  private String topping;
+  public Vanilla (String type, String size, int layers, String topping){
+    super (type, size);
+    this.topping= toppping;
+  }
+  public Vanilla (Vanilla V) {}//reem: i need to understand this!!
+  public double CalculatePrice (int size, String topping) {}
+  public double getDiscount (double prec ) {}//chnage name of method and name of argument
+  @Override 
+  public String toString () {}//close to string
+}

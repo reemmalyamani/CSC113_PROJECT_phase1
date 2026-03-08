2433d90d6facd59f4970147a33a15fda4c557d46
Reem Alyamani <alyamanireem@gmail.com>
2026-03-08 12:43:35 +0300

    Enhance Chocolate class with layers and methods
    Added properties and methods to Chocolate class.

--- Chocolate.java
+++ Chocolate.java
@@ -1,1 +1,17 @@
-public class Chocolate {}
+public class Chocolate {
+  protected int layers;
+  public Chocolate (String type, String size, int layers, boolean extra) {
+    super (type, size);
+    this.layers= layers; 
+    //i dont know what to do with cocoa
+  }//close constructor
+  public Chocolate (Chocolate c) {}
+  public CalculatePrice (int size, boolean Extra){
+    
+  }// close Calculate price
+  public double getDiscount (double prec) {} //same as comment about names in class Vanilla
+  @Override 
+  public String toString (){
+    return "" ;
+  }
+}
